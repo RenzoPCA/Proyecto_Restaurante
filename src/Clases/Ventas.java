559d813a9e2b3605d8f8c5d89860625a7527c1dc
Esther <esther.ticllav@gmail.com>
2025-03-04 01:09:34 -0500

    Añadir Ventas.java

--- src/Clases/Ventas.java
+++ src/Clases/Ventas.java
@@ -1,13 +1,41 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Clases;
 
-/**
- *
- * @author yessl
- */
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Ventas {
-    
+   private String mesero; 
+   private String detalleVenta;
+   private double total;
+   private String fechaHora;
+   
+   public Ventas(String mesero, String detalleVenta, double total){
+       this.mesero = mesero;
+       this.detalleVenta = detalleVenta;
+       this.total = total;
+       this.fechaHora = obtenerFechaHoraActual();
+   }
+   
+   private String obtenerFechaHoraActual(){
+       DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+       return LocalDateTime.now().format(formatter);
+   }
+   
+   public String getMesero(){
+       return mesero;
+   }
+   
+   public double getTotal(){
+       return total;
+   }
+   
+   public String getFechaHora(){
+       return fechaHora;
+   }
+   
+   @Override 
+    public String toString(){
+       return mesero + "," + detalleVenta + "," + total + "," + fechaHora;
+   }
 }
